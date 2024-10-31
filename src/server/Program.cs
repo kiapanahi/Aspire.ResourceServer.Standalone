9f77b59ebdbbff7e2e5f54fa981132df92001a0a
Kia Raad <kia.panahirad@gmail.com>
2024-10-31 18:36:11 +0330

    Add /info endpoint to return service information

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -14,6 +14,8 @@ var app = builder.Build();
 
 app.MapGrpcService<DashboardService>();
 
+app.MapGet("/info", (IServiceInformationProvider sip) => Results.Ok(sip.GetServiceInformation()));
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
