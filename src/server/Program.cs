5e9186b5a08f035171a29abd31dfbab13a98103b
Kia Raad <kia.panahirad@gmail.com>
2024-10-31 18:45:55 +0330

    Add gRPC reflection mapping
    Added gRPC reflection services to the application by including
    `builder.Services.AddGrpcReflection()`.
    
    Configured the application to map the `DashboardService` gRPC service
    using `app.MapGrpcService<DashboardService>()`.
    
    Added conditional mapping for the gRPC reflection service to be available
    only in the development environment.

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -6,13 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddServiceInformationProvider();
+
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.MapGrpcService<DashboardService>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 app.MapGet("/info", (IServiceInformationProvider sip) => Results.Ok(sip.GetServiceInformation()));
 
