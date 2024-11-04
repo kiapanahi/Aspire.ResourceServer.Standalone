413cde10c68beddd93bf52941ee8fff6a701a687
Kia Raad <kia.panahirad@gmail.com>
2024-11-04 16:55:00 +0330

    chore: Refactor unused lambda argument
    Discard the unused  lambda argument in service registration

--- src/server/Diagnostics/ServiceCollectionExtensions.cs
+++ src/server/Diagnostics/ServiceCollectionExtensions.cs
@@ -12,7 +12,7 @@ internal static class ServiceCollectionExtensions
     public static IServiceCollection AddServiceInformationProvider<T>(this IServiceCollection services)
         where T : IServiceInformationProvider, new()
     {
-        services.TryAddSingleton<IServiceInformationProvider>(sp => new T());
+        services.TryAddSingleton<IServiceInformationProvider>(_ => new T());
 
         return services;
     }
