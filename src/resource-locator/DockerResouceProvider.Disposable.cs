bf67fe26b73f02b30d7a7aef591b21ce6fcfed2f
Kia Raad <kia.panahirad@gmail.com>
2024-11-05 20:28:34 +0330

    chore: code cleanup

--- src/resource-locator/DockerResouceProvider.Disposable.cs
+++ src/resource-locator/DockerResouceProvider.Disposable.cs
@@ -13,15 +13,12 @@ internal sealed partial class DockerResourceProvider
                 _dockerClient?.Dispose();
             }
 
-            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-            // TODO: set large fields to null
             _disposedValue = true;
         }
     }
 
     public void Dispose()
     {
-        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
         Dispose(disposing: true);
         GC.SuppressFinalize(this);
     }
