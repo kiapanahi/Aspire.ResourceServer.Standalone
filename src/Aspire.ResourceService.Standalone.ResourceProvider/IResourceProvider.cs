176dbf7ad3fb4c91167e24fea1e2c5643da836ad
Kia Raad <kia.panahirad@gmail.com>
2024-11-07 13:35:09 +0330

    Remove separate resource provider project
    Delete the separate resource provider project.
    The following commits will move the resource provider code into the
    server project.

--- src/Aspire.ResourceService.Standalone.ResourceProvider/IResourceProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using AspireResouce = Aspire.ResourceService.Proto.V1.Resource;
-
-namespace Aspire.ResourceService.Standalone.ResourceProvider;
-
-public interface IResourceProvider : IDisposable
-{
-    Task<IEnumerable<AspireResouce>> GetResourcesAsync();
-}
