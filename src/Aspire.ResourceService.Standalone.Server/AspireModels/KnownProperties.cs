cdb835db9455742899da48610da91c466962f5ac
Kia Raad <kia.panahirad@gmail.com>
2024-11-07 13:37:13 +0330

    Add aspire models for resources and properties.
    The Aspire project uses a good practice of defining well-known properties and
    resource types in a single place. This commit adds the `KnownProperties` and
    `KnownResourceTypes` classes to the `Aspire.ResourceService.Standalone.Server` project.
    
    It also adds a `GlobalSuppressions` file to suppress warnings about the different
    namespace used in the imported files.

---
+++ src/Aspire.ResourceService.Standalone.Server/AspireModels/KnownProperties.cs
@@ -0,0 +1,40 @@
+// Copied from and inspired by .NET Aspire's KnownProperties.cs
+// https://github.com/dotnet/aspire/blob/main/src/Shared/Model/KnownProperties.cs
+
+namespace Aspire.Dashboard.Model;
+
+/// <summary>
+/// Models some known property names for known types of resources.
+/// </summary>
+/// <remarks>
+/// Used as keys in the "properties" dictionary on resource snapshots and view models.
+/// Should be compared using <see cref="StringComparer.Ordinal"/>.
+/// </remarks>
+internal static class KnownProperties
+{
+    public static class Resource
+    {
+        public const string Uid = "resource.uid";
+        public const string Name = "resource.name";
+        public const string Type = "resource.type";
+        public const string DisplayName = "resource.displayName";
+        public const string State = "resource.state";
+        public const string ExitCode = "resource.exitCode";
+        public const string CreateTime = "resource.createTime";
+        public const string StartTime = "resource.startTime";
+        public const string StopTime = "resource.stopTime";
+        public const string Source = "resource.source";
+        public const string HealthState = "resource.healthState";
+        public const string ConnectionString = "resource.connectionString";
+    }
+
+    public static class Container
+    {
+        public const string Id = "container.id";
+        public const string Image = "container.image";
+        public const string Ports = "container.ports";
+        public const string Command = "container.command";
+        public const string Args = "container.args";
+        public const string Lifetime = "container.lifetime";
+    }
+}
---
+++ src/Aspire.ResourceService.Standalone.Server/AspireModels/KnownResourceTypes.cs
@@ -0,0 +1,9 @@
+// Copied from and inspired by .NET Aspire's KnownResourceTypes.cs
+// https://github.com/dotnet/aspire/blob/main/src/Shared/Model/KnownResourceTypes.cs
+
+namespace Aspire.Dashboard.Model;
+
+internal static class KnownResourceTypes
+{
+    public const string Container = "Container";
+}
