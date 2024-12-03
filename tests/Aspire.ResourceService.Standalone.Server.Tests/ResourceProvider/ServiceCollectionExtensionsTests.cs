f09e877a4d141fa7e7c51b982d4fc54fb5437693
Kia Raad <kia.panahirad@gmail.com>
2024-12-03 18:30:06 +0330

    Add tests for resource privider DI registrations

---
+++ tests/Aspire.ResourceService.Standalone.Server.Tests/ResourceProvider/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Aspire.ResourceService.Standalone.Server.ResourceProviders;
+
+using Docker.DotNet;
+
+using FluentAssertions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Aspire.ResourceService.Standalone.Server.Tests.ResourceProvider;
+
+public class ServiceCollectionExtensionsTests
+{
+    [Fact]
+    public void AddResourceProviderShouldRegisterDockerClient()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddResourceProvider();
+        var serviceProvider = services.BuildServiceProvider();
+        var dockerClient = serviceProvider.GetService<IDockerClient>();
+
+        // Assert
+        dockerClient.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddResourceProviderShouldRegisterResourceProvider()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        services.AddResourceProvider();
+        var serviceProvider = services.BuildServiceProvider();
+        var resourceProvider = serviceProvider.GetService<IResourceProvider>();
+
+        // Assert
+        resourceProvider.Should().NotBeNull();
+        resourceProvider.Should().BeOfType<DockerResourceProvider>();
+    }
+}
