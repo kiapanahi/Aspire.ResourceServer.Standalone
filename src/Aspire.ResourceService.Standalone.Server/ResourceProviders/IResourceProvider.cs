a5dfb877d077ebe6613aecdcaad7c36124573179
Kia Raad <kia.panahirad@gmail.com>
2024-12-17 23:05:42 +0330

    Update generated proto access modifiers
    In order for the test mocks to be able to create proxies
    for the generated proto types, they need to be `public`
    and hence the `IResourceProvider` interface must be
    `public` as well

--- src/Aspire.ResourceService.Standalone.Server/ResourceProviders/IResourceProvider.cs
+++ src/Aspire.ResourceService.Standalone.Server/ResourceProviders/IResourceProvider.cs
@@ -2,7 +2,7 @@ using Aspire.ResourceService.Proto.V1;
 
 namespace Aspire.ResourceService.Standalone.Server.ResourceProviders;
 
-internal interface IResourceProvider
+public interface IResourceProvider
 {
     IAsyncEnumerable<string> GerResourceLogs(string resourceName, CancellationToken cancellationToken);
     Task<List<Resource>> GetResourcesAsync();
