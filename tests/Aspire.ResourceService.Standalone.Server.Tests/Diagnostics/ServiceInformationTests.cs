ba81b7ce9d9347864f448847b3f6a78b382d272d
Kia Raad <kia.panahirad@gmail.com>
2025-01-31 23:28:50 +0330

    Update version to 0.3.2 and adjust version test.
    - Updated version number in from `0.3.1` to `0.3.2`.
    - Modified `VersionGuard` test to expect the new version number.

--- tests/Aspire.ResourceService.Standalone.Server.Tests/Diagnostics/ServiceInformationTests.cs
+++ tests/Aspire.ResourceService.Standalone.Server.Tests/Diagnostics/ServiceInformationTests.cs
@@ -13,7 +13,7 @@ public sealed class ServiceInformationTests
     {
         IServiceInformationProvider sut = new AssemblyServiceInformationProvider();
 
-        sut.GetServiceInformation().Version.Should().Be("0.3.1");
+        sut.GetServiceInformation().Version.Should().Be("0.3.2");
         sut.GetServiceInformation().Name.Should().Be("Aspire.ResourceService.Standalone.Server");
     }
 
