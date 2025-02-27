e4e1e7c0b3bf5efb7dd809fe21dfd965d1a13d2d
Kia Raad <kia.panahirad@gmail.com>
2025-02-27 09:03:03 +0330

    [WIP] adding resource reporter

---
+++ src/Aspire.ResourceService.Standalone.Server/Reporting/ResourceSnapshotChangeType.cs
@@ -0,0 +1,14 @@
+namespace Aspire.ResourceService.Standalone.Server.Reporting;
+
+internal enum ResourceSnapshotChangeType
+{
+    /// <summary>
+    /// The object was added if new, or updated if not.
+    /// </summary>
+    Upsert,
+
+    /// <summary>
+    /// The object was deleted.
+    /// </summary>
+    Delete
+}
